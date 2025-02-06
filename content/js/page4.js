3a162b5c47dc923337e5d675d328b8ecaae273f3
CupriChicken <101941@glr.nl>
2025-02-06 13:46:31 +0000

    Fox rotated by 45 degrees :3

--- content/js/page4.js
+++ content/js/page4.js
@@ -14,17 +14,21 @@ function setup() {
 
 function draw() {
     clear();
+    
+    push();
+    translate(xPos + 270 + fox.width / 2, (height / 2 - fox.height / 2) + 100 + fox.height / 2);
+    rotate(QUARTER_PI);
+    image(fox, -fox.width / 2, -fox.height / 2);
+    pop();
+    
     image(kitty, xPos, height / 2 - kitty.height / 2); 
-    image(fox, xPos + 270, (height / 2 - fox.height / 2) + 100);
+    
     xPos += 5;
 
     if (xPos > width) {
-        if (triggered === false) {
+        if (!triggered) {
             window.location.href = "./index.html";
             triggered = true;
-        }
-        else{
-            return;
         }
     }
 }
