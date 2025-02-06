34525d9cc6bb3caecb674bb08e023d21668e1428
JeaV2 <102710@glr.nl>
2025-02-06 14:22:56 +0100

    Animate page

--- content/js/sketch.js
+++ content/js/sketch.js
@@ -1,13 +1,29 @@
 // p5 animatie
+let ufo;
+let USH = -300
 
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(windowWidth, windowHeight);
+}
+
+function preload() {
+  ufo = loadImage('../content/img/gnarp-in-ufo.png');
+  fox = loadImage('../content/img/fox.png');
 }
 
 function draw() {
   setup();
-  background(0, 255, 255);
+  background(0, 255, 255, 100);
+  image(fox, (windowWidth*0.5)-180, 600, 0, 0);
 
+  image(ufo, (windowWidth*0.5)*0.5, USH, 0, 0);
+  if (USH === 300) {
+    console.log(`Spaceship lowered to ${USH}`)
+  }
+  else {
+    USH += 1
+    console.log(USH)
+  }
 }
 
 draw()
